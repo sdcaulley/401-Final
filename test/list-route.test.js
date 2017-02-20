5e21926b6d57b88ff6b984f34990eae485e60092
sdcaulley <stephanie.caulley@gmail.com>
2017-02-20 12:48:09 -0800

    updated yml and package

---
+++ test/list-route.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const assert = chai.assert;
+const mongoose = require('mongoose');
+const app = require('../lib/app');
+const request = chai.request(app);
+
+describe('list api', () => {
+    before(() => {
+        mongoose.connection.dropDatabase();
+    });
+
+    describe('list post routes', () => {
+
+    });
+});
