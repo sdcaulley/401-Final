dbc5ee634ea0c2a57a892db4c413e30a095e2218
Clarefollett <chlaw101@gmail.com>
2017-02-21 10:45:46 -0800

    test passing for store schema :) and fixed package.json

---
+++ test/store-schema.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const Store = require('../lib/models/store-schema');
+const mongoose = require('mongoose');
+
+mongoose.Promise = Promise;
+
+describe('actor schema', () => {
+    it('example data with all fields', () => {
+        return new Store({ name: 'Fred Meyer', description: 'you can find anything at fred meyer', brand: 'Kroger', price: '$3.44', size: '12', type: 'oz' })
+            .validate(Store)
+            .then(store => console.log(store))
+            .catch(err => { throw err; });
+    });
+});
