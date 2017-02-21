4c48ee64a4affe0904af945c38e89c478e645050
Clarefollett <chlaw101@gmail.com>
2017-02-21 11:44:00 -0800

    got some more tests passing! whoo!

--- test/store-schema.test.js
+++ test/store-schema.test.js
@@ -1,10 +1,23 @@
 //const assert = require('chai').assert;
 const Store = require('../lib/models/store-schema');
+const testInvalid = require('./test-invalid')(Store);
 const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 
 describe('store schema', () => {
+    it('requires name', () => {
+        return testInvalid({ brand: 'Kroger' });
+    });
+    it('requires brand', () => {
+        return testInvalid({ price: '$3.88' });
+    });
+    it('requires price', () => {
+        return testInvalid({ name: 'Fred Meyer' });
+    });
+    it('is valid with name, brand, and price', () => {
+        return new Store({ name: 'Fred Meyer', brand: 'Kroger', price: '$3.88' }).validate();
+    });
     it('example data with all fields', () => {
         const data = { name: 'Fred Meyer', brand: 'Kroger', price: '$3.44' };
         //requires all fields except unit
