64625badda32235e67a3f6dbbfa5871ea6115184
mericks <mericks@users.noreply.github.com>
2017-02-21 12:42:11 -0800

    deleted drop database from file (action performed in _before)

--- test/list-route.test.js
+++ test/list-route.test.js
@@ -2,14 +2,10 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
-const mongoose = require('mongoose');
 const app = require('../lib/app');
 const request = chai.request(app);
 
 describe('list api', () => {
-    before(() => {
-        mongoose.connection.dropDatabase();
-    });
 
     let listOne = {
         name: 'List One',
