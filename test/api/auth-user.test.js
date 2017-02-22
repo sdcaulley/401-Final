d80b21498470ad7cd1eea7f23adbf56d979a68fd
mericks <mericks@users.noreply.github.com>
2017-02-22 15:20:17 -0800

    auth tests moved to api folder

--- test/api/auth-user.test.js
+++ test/api/auth-user.test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
-const app = require('../lib/app');
+const app = require('../../lib/app');
 
 describe('authenticate credentials', () => {
 
