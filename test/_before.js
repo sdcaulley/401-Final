90184d85878bc36ae26ee7c897c070cf24b0a281
mericks <mericks@users.noreply.github.com>
2017-02-22 15:19:52 -0800

    removed load data function from shopper-test db connection

--- test/_before.js
+++ test/_before.js
@@ -1,26 +1,6 @@
 process.env.MONGODB_URI = 'mongodb://localhost:27017/shopper-test';
 require('../lib/connection');
-const childProcess = require('child_process');
 const mongoose = require('mongoose');
 
 
-function loadData(collection) {
-    return `mongoimport --db shopper-test --collection ${collection} --file data/${collection}.json --jsonArray`;
-}
-
-before(done => {
-    mongoose.connection.dropDatabase();
-    childProcess.exec(loadData('users'), err => {
-        if(err) return done(err);
-        childProcess.exec(loadData('accounts'), err => {
-            if(err) return done(err);
-            childProcess.exec(loadData('stores'), err => {
-                if(err) return done(err);
-                childProcess.exec(loadData('items'), err => {
-                    if(err) return done(err);
-                    childProcess.exec(loadData('lists'), done);
-                });
-            });
-        });
-    });
-});
+before(() => mongoose.connection.dropDatabase());
