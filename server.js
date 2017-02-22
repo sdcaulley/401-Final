96d7ad5252db5e59422355889d07c09ffa9de524
sdcauley <stephanie.caulley@gmail.com>
2017-02-22 11:18:43 -0800

    added mongo initialization

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./lib/app');
 const http = require('http');
 const PORT = process.env.PORT || 3000;
+require('./lib/connection');
+
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
