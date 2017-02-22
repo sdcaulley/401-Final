ba020ad3bc776f6fafb949e7ccd46c5efca915bb
mericks <mericks@users.noreply.github.com>
2017-02-22 11:54:56 -0800

    corrected path to app.js

--- test/api/lists-route.test.js
+++ test/api/lists-route.test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
-const app = require('../lib/app');
+const app = require('../../lib/app');
 const request = chai.request(app);
 
 describe('list api', () => {
