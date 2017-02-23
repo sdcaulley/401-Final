e25149937aef9233c4e4b64903ead6d7112475d9
sdcaulley <stephanie.caulley@gmail.com>
2017-02-23 09:46:37 -0800

    changing _before

--- test/_before.js
+++ test/_before.js
@@ -2,5 +2,4 @@ process.env.MONGODB_URI = 'mongodb://localhost:27017/shopper-test';
 require('../lib/connection');
 const mongoose = require('mongoose');
 
-
 before(() => mongoose.connection.dropDatabase());
